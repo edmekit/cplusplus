46eae687b2e8a1cedfc5f4abb71dd65161cf86ec
Edmarc Mekitpekit <mekitpekit.eggmarc@gmail.com>
2025-07-13 14:07:07 +0800

    up

--- hellooo.cpp
+++ hellooo.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
 
 using namespace std;
+ int i, j, s
 
 int main() {
+
+
     for (int i =0; i < 5; i++) 
     {
         for (int j=0; j < 5; j++)
@@ -11,4 +14,9 @@ int main() {
         } cout << endl;
     } 
     return 0;
+}
+
+
+int s(){
+    
 }

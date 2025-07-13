37e237bd7cdd59b16395002d273db91d5895c4d1
Edmarc Mekitpekit <mekitpekit.eggmarc@gmail.com>
2025-07-13 15:16:47 +0800

    add user input

--- hellooo.cpp
+++ hellooo.cpp
@@ -8,18 +8,23 @@ int s(string text);
 int main() {
 
     s("Go to hell");
-    for (i =0; i < 5; i++) 
-    {
-        for (j=0; j < 5; j++)
-        {
-            cout << "*";
-        } cout << endl;
-    } 
+
     return 0;
 }
 
 
 int s(string text){
+    int num;
     string name = "James";
     cout << text << ", " << name << endl;
+    cout << "What do you want"<< endl;
+    cin >> num;
+    for (i =0; i < num; i++) 
+    {
+        for (j=0; j < num; j++)
+        {
+            cout << " * ";
+        }cout << endl;
+    } 
 }
+
